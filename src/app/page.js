aaea26a5389d00a1cef830d4bd5c3064400209e1
bkanhu <bkanhu@skentino.com>
2024-03-06 15:43:55 +0530

    Update NextJS to 14.1.2 and add vs code editor config for workspace.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
+ 
 
 export default function Home() {
   return (
     <>
       <h1>Project Starter</h1>
-      <p>we'll</p>
+      <p>This is a starter project</p>
     </>
   );
 }
--- src/components/UI/Label.js
+++
@@ -1,5 +0,0 @@
-const Label = () => {
-  return <></>;
-};
-
-export default Label;
